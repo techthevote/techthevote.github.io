3af23f505ae26c78edbe2c5819a74cf9f99b1176
Patrick Dugan <dugan.pat@gmail.com>
2016-09-25 22:21:04 -0700

    fixed busted js
    fixed incorrectly linked js

--- assets/js/tech-the-vote.js
+++ assets/js/tech-the-vote.js
@@ -3,3 +3,21 @@ $('.arrow-img').click(function(event) {
     var n = $(document).height();
     $('html, body').animate({ scrollTop: 650}, 2000);
 });
+
+$('.chapter-1').click(function(event) {
+    event.preventDefault();
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 510}, 1500);
+});
+
+$('.chapter-2').click(function(event) {
+    event.preventDefault();
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 1350}, 1500);
+});
+
+$('.chapter-3').click(function(event) {
+    event.preventDefault();
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 2100}, 1500);
+});
